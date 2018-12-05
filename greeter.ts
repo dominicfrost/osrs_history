b21f7c25f7b119a94cc23e405dda4b008fe53580
Dominic Frost <frostationdevice@gmail.com>
2018-12-04 17:16:20 -0800

    first commit

---
+++ greeter.ts
@@ -0,0 +1,9 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+document.body.innerHTML = greeter(user);
+
+let highscoreUrl = "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=iron_frawsti"
